b89c459cf3baa315e5e949b069d9285f3cecf543
Dieter Coopman <dieter@deltasolutions.be>
2023-03-09 22:37:49 +0100

    build mailbody
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/Microsoftgraph.php
+++ src/Microsoftgraph.php
@@ -2,10 +2,12 @@
 
 namespace LLoadout\Microsoftgraph;
 
+use Illuminate\Http\Request;
 use Illuminate\Mail\Mailable;
-use JetBrains\PhpStorm\NoReturn;
+use Illuminate\Mail\Mailables\Address;
 use Laravel\Socialite\Facades\Socialite;
 use Microsoft\Graph\Graph;
+use SplFileObject;
 
 class Microsoftgraph
 {
@@ -14,53 +16,108 @@ class Microsoftgraph
         return Socialite::driver('microsoft')->redirect();
     }
 
-    public function callback()
+    public function callback(Request $request): void
     {
-        $user = Socialite::driver('microsoft')->user();
-        session(['token' => $user->token]);
+        $user = (array) Socialite::driver('microsoft')->user();
+        $request->session()->put('token', $user['token']);
     }
 
-    #[NoReturn] public function sendMail(Mailable $mailable)
+    public function sendMail(Mailable $mailable): void
     {
         $graph = new Graph();
         $graph->setAccessToken(session('token'));
 
-        $user = (object)$graph->createRequest('GET', '/me')->execute()->getBody();
+        $user = (object) $graph->createRequest('GET', '/me')->execute()->getBody();
+        $body = $this->getBody($mailable, $user);
 
+        $graph->createRequest('POST', '/me/sendMail')->attachBody($body)->execute();
+    }
+
+    protected function getBody($mailable, $user)
+    {
         $envelope = $mailable->envelope();
-        $to       = $envelope->to;
-        $html     = $mailable->render();
+        $from = new Address($user->mail, $user->displayName);
 
-        $mailBody = ['Message' => [
-            'subject'      => 'Test Email',
-            'body'         => [
+        return array_filter([
+            'message' => [
+                'subject' => $envelope->subject,
+                'sender' => $this->formatRecipients($from)[0],
+                'from' => $this->formatRecipients($from)[0],
+                'replyTo' => $this->formatRecipients($envelope->replyTo),
+                'toRecipients' => $this->formatRecipients($envelope->to),
+                'ccRecipients' => $this->formatRecipients($envelope->cc),
+                'bccRecipients' => $this->formatRecipients($envelope->bcc),
+                'body' => $this->getContent($mailable),
+                'attachments' => $this->toAttachmentCollection($mailable->attachments),
+            ],
+        ]);
+    }
+
+    protected function formatRecipients($recipients): array
+    {
+        $addresses = [];
+
+        if (! $recipients) {
+            return $addresses;
+        }
+
+        if (! is_countable($recipients)) {
+            $addresses[] = [
+                'emailAddress' => [
+                    'name' => $recipients->name,
+                    'address' => $recipients->address,
+                ],
+            ];
+
+            return $addresses;
+        }
+
+        foreach ($recipients as $address) {
+            $addresses[] = [
+                'emailAddress' => [
+                    'name' => $address->name,
+                    'address' => $address->address,
+                ],
+            ];
+        }
+
+        return $addresses;
+    }
+
+    private function getContent(Mailable $mailable): array
+    {
+        if (! empty($mailable->render())) {
+            $content = [
                 'contentType' => 'html',
-                'content'     => $html,
-            ],
-            'sender'       => [
-                'emailAddress' => [
-                    'name'    => $user->displayName,
-                    'address' => $user->mail,
-                ],
-            ],
-            'from'         => [
-                'emailAddress' => [
-                    'name'    => $user->displayName,
-                    'address' => $user->mail,
-                ],
-            ],
-            'toRecipients' => [
-                [
-                    'emailAddress' => [
-                        'name'    => $to['name']->address,
-                        'address' => $to['address']->address,
+                'content' => $mailable->render(),
+            ];
+        }
 
-                    ],
-                ],
-            ],
-        ],
-        ];
+        return $content ?? '';
+    }
 
-        $graph->createRequest('POST', '/me/sendMail')->attachBody($mailBody)->execute();
+    protected function toAttachmentCollection($attachments): array
+    {
+        $collection = [];
+
+        foreach ($attachments as $file) {
+            $file = new SplFileObject($file['file'], 'r');
+            $body = $file->fread($file->getSize());
+            $imgdata = base64_decode($body);
+            $f = finfo_open();
+            $contentType = finfo_buffer($f, $imgdata, FILEINFO_MIME_TYPE);
+
+            $collection[] = [
+                'name' => $file->getFilename(),
+                'contentId' => $file->getInode().'@lloadout.graph',
+                'contentBytes' => base64_encode($body),
+                'contentType' => $contentType,
+                'size' => strlen($body),
+                '@odata.type' => '#microsoft.graph.fileAttachment',
+                'isInline' => true,
+            ];
+        }
+
+        return $collection;
     }
 }

e2b38e2f9fa661cbb1e47ef23eaa589e6a03291f
Dieter Coopman <dieter@deltasolutions.be>
2023-06-13 22:12:42 +0200

    extend teams class
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/Teams.php
+++ src/Teams.php
@@ -8,24 +8,38 @@ class Teams
 {
     use \LLoadout\Microsoftgraph\Traits\Authenticate;
 
+    public function graph(): Graph
+    {
+        return (new Graph())->setAccessToken($this->getAccessToken());
+    }
     public function getTeams(): array
     {
-        $graph = (new Graph())->setAccessToken($this->getAccessToken());
+        return $this->graph()->createRequest('GET', '/me/joinedTeams')->execute()->getBody()['value'];
+    }
 
-        return $graph->createRequest('GET', '/me/joinedTeams')->execute()->getBody();
+    public function getChats(): array
+    {
+        return $this->graph()->createRequest('GET', '/me/chats')->execute()->getBody()['value'];
+    }
+
+    public function getMembersInChat($chat): array
+    {
+        return $this->graph()->createRequest('GET', '/chats/'.$chat['id'].'/members')->execute()->getBody()['value'];
+    }
+
+    public function getContacts(): array
+    {
+        return $this->graph()->createRequest('GET', '/me/contacts')->execute()->getBody();
     }
 
     public function getChannels($team): array
     {
-        $graph = (new Graph())->setAccessToken($this->getAccessToken());
-
-        return $graph->createRequest('GET', '/teams/'.$team.'/channels')->execute()->getBody();
+        return $this->graph()->createRequest('GET', '/teams/'.$team.'/channels')->execute()->getBody();
     }
-    public function send($team, $messsage): array
+    public function send($chat, $messsage): array
     {
-        $graph = (new Graph())->setAccessToken($this->getAccessToken());
         $data = json_decode('{"body": {"contentType": "html","content": "'.$messsage.'"}}');
 
-        return $graph->createRequest('POST', '/chats/'.$team.'/messages')->attachBody($data)->execute()->getBody();
+        return $this->graph()->createRequest('POST', '/chats/'.$chat['id'].'/messages')->attachBody($data)->execute()->getBody();
     }
 }

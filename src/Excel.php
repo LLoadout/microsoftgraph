424a6c22a38b1c18f13f372ec04f243ac904942b
Dieter Coopman <dieter@deltasolutions.be>
2023-06-13 22:12:52 +0200

    extend excel class
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/Excel.php
+++ src/Excel.php
@@ -14,6 +14,13 @@ class Excel
 
     private mixed  $excelSession;
 
+    /**
+     * Load an excel file from onedrive
+     * @param $file
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Microsoft\Graph\Exception\GraphException
+     */
     public function loadFile($file)
     {
         $graph = (new Graph())->setAccessToken($this->getAccessToken());
@@ -25,23 +32,40 @@ class Excel
         $this->graph = $graph;
     }
 
+    /**
+     * set the values of a cell or range of cells
+     * @param $cell
+     * @param $values
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Microsoft\Graph\Exception\GraphException
+     */
     public function setCellValues($cell, $values)
     {
         $url = '/me/drive/items/'.$this->fileId.'/workbook/worksheets/{00000000-0001-0000-0000-000000000000}/range(address=\''.$cell.'\')';
         $this->graph->createRequest('patch', $url)->addHeaders(['workbook-session-id' => $this->excelSession])->attachBody(['values' => $values])->execute();
     }
 
-
+    /**
+     * get the values of a cell or range of cells
+     * @param $cell
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Microsoft\Graph\Exception\GraphException
+     */
     public function getCell($cell)
     {
         $url = '/me/drive/items/'.$this->fileId.'/workbook/worksheets/{00000000-0001-0000-0000-000000000000}/range(address=\''.$cell.'\')';
-
         return $this->graph->createRequest('get', $url)->addHeaders(['workbook-session-id' => $this->excelSession])->execute()->getBody();
     }
 
+    /**
+     * Calculate the workbook
+     * @return void
+     */
     public function recalculate()
     {
-        $url = config('services.office.api_url').'/me/drive/items/01CPKWSS4XOQSWSLYELBEKGUFVUI7JQVG7/workbook/application/calculate';
+        $url = config('services.office.api_url').'/me/drive/items/'.$this->fileId.'/workbook/application/calculate';
         $this->doPost('post', $url, []);
     }
 }

af10c793329ac6042d95c4be5bcc10da0bce45a3
Dieter Coopman <dieter@deltasolutions.be>
2024-03-28 14:44:18 +0100

    redirect to home route on callback
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/Authenticate.php
+++ src/Authenticate.php
@@ -16,13 +16,14 @@ class Authenticate
         return Socialite::driver('microsoft')->with(['scope' => '.default', 'prompt' => 'consent'])->redirect();
     }
 
-    public function callback(): void
+    public function callback()
     {
         if (request()->has('error')) {
             MicrosoftGraphErrorReceived::dispatch(encrypt((object)['error' => request('error'), 'error_description' => request('error_description')]));
         } else {
             $tokenData = Http::asForm()->post('https://login.microsoftonline.com/' . config('services.microsoft.tenant_id') . '/oauth2/token', $this->getTokenFields(request('code')))->object();
             $this->dispatchCallbackReceived($tokenData);
+            return redirect('/');
         }
     }
 }

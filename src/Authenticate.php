62ad1f457e1f33dd3f53f888571e18fa9376e3c8
Dieter Coopman <dieter@deltasolutions.be>
2025-02-21 16:16:18 +0100

    add documentation for authenticate
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/Authenticate.php
+++ src/Authenticate.php
@@ -7,15 +7,38 @@ use Laravel\Socialite\Facades\Socialite;
 use LLoadout\Microsoftgraph\EventListeners\MicrosoftGraphErrorReceived;
 use LLoadout\Microsoftgraph\Traits\Authenticate as AuthTrait;
 
+/**
+ * Class Authenticate
+ *
+ * Handles Microsoft Graph API authentication using OAuth2 flow
+ *
+ * @package LLoadout\Microsoftgraph
+ */
 class Authenticate
 {
     use AuthTrait;
 
+    /**
+     * Initiates the OAuth2 authentication flow with Microsoft
+     *
+     * Redirects user to Microsoft login page with required scopes and consent prompt
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
     public function connect()
     {
         return Socialite::driver('microsoft')->with(['scope' => '.default', 'prompt' => 'consent'])->redirect();
     }
 
+    /**
+     * Handles the OAuth2 callback from Microsoft
+     *
+     * Processes the authentication response, either:
+     * - Dispatches error event if authentication failed
+     * - Retrieves access token and dispatches success event if authentication succeeded
+     *
+     * @return \Illuminate\Http\RedirectResponse|void
+     */
     public function callback()
     {
         if (request()->has('error')) {
